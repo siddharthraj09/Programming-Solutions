f4b7a61414e55f66aea49017ab591d8ae4c706d1
Siddharth Raj <siddharthraj34@yahoo.com>
2021-11-28 21:43:25 +0530

    pattern

---
+++ Pattern/pattern_17.java
@@ -0,0 +1,47 @@
+
+import java.util.Scanner;
+
+public class pattern_17 {
+    public static void pattern(int n){
+        
+        int nst=1,nsp=n/2;
+        for(int r=1;r<=n;r++){
+            //int count=0;
+
+            for(int csp=1;csp<=nsp;csp++){
+            if(r==n/2+1)
+            {
+                System.out.print("*\t");
+
+            }
+            else {
+                System.out.print("\t");
+
+            }
+            }
+            for(int cst=1;cst<=nst;cst++){
+                System.out.print("*\t");
+                
+            }
+
+           if(r<=n/2){
+                    nst++;
+                }
+                else{
+                    nst--;
+              }
+                
+                System.out.println();
+            }
+       }
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int x=scn.nextInt();
+        pattern(x);
+        scn.close();
+
+    }
+
+    
+}
