e744b79cbf8b0097b1e6af9b52e3080ef489f799
Siddharth Raj <46163912+siddharthraj09@users.noreply.github.com>
2021-11-29 11:59:36 +0530

    Update pattern_15.java

--- Pattern/pattern_15.java
+++ Pattern/pattern_15.java
@@ -1,9 +1,9 @@
 // Sample Output
-// 		    1	
-// 	     2	3	2	
-//   3	 4	5	4	3	
-// 	     2	3	2	
-// 		    1	
+// 		        1	
+// 	         2	3	2	
+//        3	 4	5	4 3	
+// 	         2	3	2	
+// 		        1	
 
 import java.util.Scanner;
 
