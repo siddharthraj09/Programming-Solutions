7b8dfb1de63d6a2d4a483cd2d728294bae2e6033
Siddharth Raj <siddharthraj34@yahoo.com>
2021-12-21 13:28:52 +0530

    Print in Decreasing order

---
+++ Recursion/printDecreasing.java
@@ -0,0 +1,28 @@
+// 1. You are given a positive number n. 
+// 2. You are required to print the counting from n to 1.
+// 3. You are required to not use any loops. Complete the body of print Decreasing function to achieve it.
+
+// Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+import java.util.Scanner;
+
+public class printDecreasing {
+
+    public static Scanner scn=new Scanner(System.in);
+    public static void printDecr(int n) {
+        if (n==0)
+        return;
+
+        System.out.println(n);
+        printDecr(n-1);
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int n=scn.nextInt();
+        printDecr(n);
+    }
+    
+}
