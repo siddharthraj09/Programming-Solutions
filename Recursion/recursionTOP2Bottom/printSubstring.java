f6cea9221cfd94305bdf48658f84a1a7832f08b9
siddharthraj09 <siddharthraj34@yahoo.com>
2022-01-02 19:17:46 +0530

    getSS void

---
+++ Recursion/recursionTOP2Bottom/printSubstring.java
@@ -0,0 +1,22 @@
+package recursionTOP2Bottom;
+import java.util.*;
+public class printSubstring {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String s = scn.next();
+        printSS(s, "");
+    }
+
+    public static void printSS(String str, String ans) {
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+        char ch = str.charAt(0);    //a
+        String ros = str.substring(1);  //bc
+        printSS(ros, ans + ch);
+        printSS(ros, ans);
+        
+    }
+    
+}
